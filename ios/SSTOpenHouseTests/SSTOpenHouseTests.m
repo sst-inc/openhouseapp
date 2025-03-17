16302e32e5a59a040f9dc9e17155a4a836623f8b
Goh Min Wen, Ted <ilovefirebase4@gmail.com>
2025-03-17 14:15:37 +0800

    version upgrade, icon change, renaming

--- ios/SSTOpenHouseTests/SSTOpenHouseTests.m
+++ ios/SSTOpenHouseTests/SSTOpenHouseTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SSTOpenHouse2024Tests : XCTestCase
+@interface SSTOpenHouseTests : XCTestCase
 
 @end
 
-@implementation SSTOpenHouse2024Tests
+@implementation SSTOpenHouseTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
